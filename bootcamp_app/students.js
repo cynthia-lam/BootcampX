d813b76cbd2d85618e2a8437f44cf33ab3ce9d68
Cynthia <cynthia-lam@outlook.com>
2024-04-14 18:19:28 -0400

    Update students.js and teachers.js to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -13,10 +13,10 @@ pool
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+WHERE cohorts.name LIKE '$1'
+LIMIT $2;
 `
-  )
+, [process.argv[2], process.argv[3] || 5])
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
